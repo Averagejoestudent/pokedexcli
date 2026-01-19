bfa055c06ca45a80d908f0a5941df5c631116632
ThePrimeagen <zoimmoiz@gmail.com>
2026-01-19 14:39:10 +0500

    Second commit for Go project

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		my_first_line := scanner.Text()
+		my_first_word := strings.Fields(strings.ToLower(my_first_line))
+		fmt.Println("Your command was:", my_first_word[0])
+	}
 }
