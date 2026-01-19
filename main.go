d14e0f08b732ec1cb129a5834aa131fa40a3e6aa
ThePrimeagen <zoimmoiz@gmail.com>
2026-01-19 14:46:59 +0500

    Third commit for Go project

--- main.go
+++ main.go
@@ -1,19 +1,9 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"os"
-	"strings"
+	
 )
 
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	for {
-		fmt.Print("Pokedex > ")
-		scanner.Scan()
-		my_first_line := scanner.Text()
-		my_first_word := strings.Fields(strings.ToLower(my_first_line))
-		fmt.Println("Your command was:", my_first_word[0])
-	}
+	startfunc()
 }
--- repl.go
+++ repl.go
@@ -1,8 +1,22 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
+
+func startfunc() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		my_first_line := scanner.Text()
+		my_first_word := cleanInput(my_first_line)
+		fmt.Println("Your command was:", my_first_word[0])
+	}
+}
 
 func cleanInput(text string) []string {
 	var my_string []string
